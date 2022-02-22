0197e865b13a208ec700646784cebdf894e81aeb
ShushoRiv <73494815+A01655181@users.noreply.github.com>
2022-02-22 13:00:38 -0600

    Add files via upload

---
+++ misfunciones.js
@@ -0,0 +1,5 @@
+var hola = "Hola Mundo"
+function procesarForm(){ 
+    var nombre = document.getElementById("nombre").value;  
+    document.getElementById("salida").innerHTML = "Gracias " + nombre + " Nos vemos pronto :D";    
+}
